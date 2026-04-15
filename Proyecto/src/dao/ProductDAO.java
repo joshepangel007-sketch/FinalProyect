35569fa41599b436c68c1ef0009ca72394f14999
joshepangel007-sketch <joshepangel007@gmail.com>
2026-04-15 17:50:52 -0500

    Query de insertar, listo

--- Proyecto/src/dao/ProductDAO.java
+++ Proyecto/src/dao/ProductDAO.java
@@ -1,8 +1,24 @@
 package dao;
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
 import model.Product;
 public class ProductDAO {
+
+     public void insertProduct(Product product) throws SQLException {
+        String sql = "INSERT INTO products (name, price, quantity, category) VALUES (?, ?, ?, ?)";
+        
+        try (Connection conn = db.ConnectionDB.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            
+            pstmt.setString(1, product.getname());
+            pstmt.setDouble(2, product.getprice());
+            pstmt.setInt(3, product.getquantity());
+            pstmt.setString(4, product.getcategory());
+            pstmt.executeUpdate();
+        }
+    }
+
     
 }
+
+
+
