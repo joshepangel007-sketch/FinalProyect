a8abbddcf6005efab46acc09509ade77e8f6bb0e
joshepangel007-sketch <joshepangel007@gmail.com>
2026-04-14 23:20:36 -0500

    Inicio de el product, se crearon los campos de la base de datos

---
+++ Proyecto/src/service/ProductServcice.java
@@ -0,0 +1,11 @@
+package service;
+
+public class ProductServcice {
+    private int id; 
+    private String name; 
+    private double price; 
+    private int quantity;
+    private String category; 
+
+    
+}
