ecb3566458b3c6140ab1ed1b83f1f2147e795da4
joshepangel007-sketch <joshepangel007@gmail.com>
2026-04-16 03:54:18 -0500

    Despues de estar sentado desde las 11 pm, logre hacer que el jdbc funcionara

--- Proyecto/src/db/ConnectionDB.java
+++ Proyecto/src/db/ConnectionDB.java
@@ -5,15 +5,18 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionDB {
-    private static final String URL = "jdbc:mysql://100.96.142.77:3306/inventory_db";
-
-    // Metodo que recibe usuario y contraseña para conectarse a la base de datos
-    public static Connection getConnection(String user, String password) throws SQLException {
+    private static final String URL = "jdbc:mysql://localhost:3306/inventory_db";
+    private static final String USER = "jfalcon";
+    private static final String PASSWORD = "";
+    
+    public static Connection getConnection() throws SQLException {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            return DriverManager.getConnection(URL, user, password);
+            return DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (ClassNotFoundException e) {
             throw new SQLException("Driver no encontrado", e);
         }
     }
 }
+
+
