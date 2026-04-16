32a5f97e2553e89e5f2ef8286457874cb7faf2eb
joshepangel007-sketch <joshepangel007@gmail.com>
2026-04-16 02:39:28 -0500

    Inicio de correcciones

--- Proyecto/src/dao/ProductDAO.java
+++ Proyecto/src/dao/ProductDAO.java
@@ -1,25 +1,27 @@
 package dao;
+
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import model.Product;
-public class ProductDAO {
 
-     public void insertProduct(Product product) throws SQLException {
+public class ProductDAO {
+    
+    public void insertProduct(Product product) throws SQLException {
         String sql = "INSERT INTO products (name, price, quantity, category) VALUES (?, ?, ?, ?)";
         
         try (Connection conn = db.ConnectionDB.getConnection();
              PreparedStatement pstmt = conn.prepareStatement(sql)) {
             
-            pstmt.setString(1, product.getname());
-            pstmt.setDouble(2, product.getprice());
-            pstmt.setInt(3, product.getquantity());
-            pstmt.setString(4, product.getcategory());
+            pstmt.setString(1, product.getName());
+            pstmt.setDouble(2, product.getPrice());
+            pstmt.setInt(3, product.getQuantity());
+            pstmt.setString(4, product.getCategory());
             pstmt.executeUpdate();
         }
     }
     
-    public List<Product> getAllProducts() throws SQLException {//Hace un select de toda la base de datos
+    public List<Product> getAllProducts() throws SQLException {
         List<Product> products = new ArrayList<>();
         String sql = "SELECT * FROM products";
         
@@ -30,17 +32,17 @@ public class ProductDAO {
             while (rs.next()) {
                 Product p = new Product();
                 p.setId(rs.getInt("id"));
-                p.setname(rs.getString("name"));
-                p.setprice(rs.getDouble("price"));
-                p.setquantity(rs.getInt("quantity"));
-                p.setcategory(rs.getString("category"));
+                p.setName(rs.getString("name"));
+                p.setPrice(rs.getDouble("price"));
+                p.setQuantity(rs.getInt("quantity"));
+                p.setCategory(rs.getString("category"));
                 products.add(p);
             }
         }
         return products;
     }
-
-    public Product getProductById(int id) throws SQLException {//Busca por id del producto
+    
+    public Product getProductById(int id) throws SQLException {
         String sql = "SELECT * FROM products WHERE id = ?";
         
         try (Connection conn = db.ConnectionDB.getConnection();
@@ -52,32 +54,32 @@ public class ProductDAO {
             if (rs.next()) {
                 Product p = new Product();
                 p.setId(rs.getInt("id"));
-                p.setname(rs.getString("name"));
-                p.setprice(rs.getDouble("price"));
-                p.setquantity(rs.getInt("quantity"));
-                p.setcategory(rs.getString("category"));
+                p.setName(rs.getString("name"));
+                p.setPrice(rs.getDouble("price"));
+                p.setQuantity(rs.getInt("quantity"));
+                p.setCategory(rs.getString("category"));
                 return p;
             }
             return null;
         }
     }
-
-    public void updateProduct(Product product) throws SQLException {//Actualiza los valores por id 
+    
+    public void updateProduct(Product product) throws SQLException {
         String sql = "UPDATE products SET name = ?, price = ?, quantity = ?, category = ? WHERE id = ?";
         
         try (Connection conn = db.ConnectionDB.getConnection();
              PreparedStatement pstmt = conn.prepareStatement(sql)) {
             
-            pstmt.setString(1, product.getname());
-            pstmt.setDouble(2, product.getprice());
-            pstmt.setInt(3, product.getquantity());
-            pstmt.setString(4, product.getcategory());
+            pstmt.setString(1, product.getName());
+            pstmt.setDouble(2, product.getPrice());
+            pstmt.setInt(3, product.getQuantity());
+            pstmt.setString(4, product.getCategory());
             pstmt.setInt(5, product.getId());
             pstmt.executeUpdate();
         }
     }
     
-    public void deleteProduct(int id) throws SQLException { //Elimina un registro
+    public void deleteProduct(int id) throws SQLException {
         String sql = "DELETE FROM products WHERE id = ?";
         
         try (Connection conn = db.ConnectionDB.getConnection();
@@ -88,6 +90,3 @@ public class ProductDAO {
         }
     }
 }
-
-
-
